e891fa401063e1a60fc6a5cc1841a9d6339a93a0
Aidan Zealley <aidan.zealley@sky.uk>
2017-11-21 15:25:44 +0000

    add sticky-images

--- sticky-images/js/parallax.js
+++ sticky-images/js/parallax.js
@@ -1,17 +1,78 @@
 NodeList.prototype.forEach = Array.prototype.forEach;
 
-function adjustSubjectPosition(parallaxWindow) {
+function setProperties(subject, frameDimensions, property, defaults) {
+
+  switch (property) {
+    case 'fade':
+    subject.style.cssText = 'opacity: ' + -(frameDimensions.top/50)*defaults.multiplier;
+    break;
+
+    case 'transform':
+    subject.style.transform = '';
+
+    defaults.actions.forEach(function(action) {
+
+      switch (action) {
+        case 'translateY':
+        subject.style.transform += 'translateY(' + defaults.direction*(frameDimensions.top*defaults.multiplier) + 'px)';
+        break;
+
+        case 'translateX':
+        subject.style.transform += 'translateX(' + defaults.direction*(frameDimensions.top*defaults.multiplier) + 'px)';
+        break;
+
+        case 'rotate':
+        subject.style.transform += 'rotate(' + defaults.direction*(frameDimensions.top*defaults.multiplier) + 'deg)';
+        break;
+
+        case 'scale':
+        subject.style.transform += 'scale(' + ((frameDimensions.top/100)*defaults.multiplier) + ')';
+        break;
+      }
+    });
+    break;
+
+  }
+
+}
+
+function adjustSubjectPosition(parallaxFrame) {
+
+  var defaults = {
+    properties: ['transform'],
+    actions: ['translateY'],
+    multiplier: 0.5,
+    direction: -1
+  }
 
   var height = window.innerHeight;
-  var parallexWindowDimensions = parallaxWindow.getBoundingClientRect();
+  var parallaxFrameDimensions = parallaxFrame.getBoundingClientRect();
 
-  if (parallexWindowDimensions.top < height) {
-    var parallaxSubjects = parallaxWindow.querySelectorAll('[data-role="parallax-subject"]');
+  if (parallaxFrameDimensions.top < height) {
+    var parallaxSubjects = parallaxFrame.querySelectorAll('[data-role="parallax-subject"]');
 
     parallaxSubjects.forEach(function(parallaxSubject) {
-      var multiplier = parallaxSubject.dataset.multiplier;
+      if (parallaxSubject.getAttribute('data-multiplier') != null) {
+        defaults.multiplier = parallaxSubject.getAttribute('data-multiplier');
+      }
+      if (parallaxSubject.getAttribute('data-properties') != null) {
+        defaults.properties = parallaxSubject.getAttribute('data-properties').split(',');
+      }
+      if (parallaxSubject.getAttribute('data-actions') != null) {
+        defaults.actions = parallaxSubject.getAttribute('data-actions').split(',');
+      }
+      if (parallaxSubject.getAttribute('data-direction') != null) {
+        var direction = parallaxSubject.getAttribute('data-direction');
+        if (direction = "backwards") {
+          defaults.direction = 1;
+        }
+      }
 
-      parallaxSubject.style.cssText = 'transform: translateY(' + (-parallexWindowDimensions.top*multiplier) + 'px)';
+      defaults.properties.forEach(function(property) {
+
+        setProperties(parallaxSubject, parallaxFrameDimensions, property, defaults);
+
+      });
 
     });
   }
@@ -23,14 +84,14 @@ function triggerParallax() {
   function findScrollPosition() {
 
     var height = window.innerHeight;
-    var parallaxWindows = document.querySelectorAll('[data-role="parallax-window"]');
+    var parallaxFrames = document.querySelectorAll('[data-role="parallax-frame"]');
 
-    parallaxWindows.forEach(function(parallaxWindow) {
+    parallaxFrames.forEach(function(parallaxFrame) {
 
-      var parallaxWindowDimensions = parallaxWindow.getBoundingClientRect();
+      var parallaxFrameDimensions = parallaxFrame.getBoundingClientRect();
 
-      if (parallaxWindowDimensions.top < height && (parallaxWindowDimensions.top + parallaxWindowDimensions.height) > 0 ) {
-        adjustSubjectPosition(parallaxWindow);
+      if (parallaxFrameDimensions.top < height && (parallaxFrameDimensions.top + parallaxFrameDimensions.height) > 0 ) {
+        adjustSubjectPosition(parallaxFrame);
       }
 
     });
