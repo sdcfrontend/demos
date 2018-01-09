eabdc0df1bfec694e869f4633a2ef3ee514fc894
Hawkin, Adam (Associate Software Developer) <adam.hawkin@sky.uk>
2018-01-09 11:08:16 +0000

    Zip src

--- custom-story-grid/src/test/components/custom-story-picker.test.js
+++
@@ -1,7 +0,0 @@
-import CustomStoryPicker from '../../src/components/custom-story-picker';
-
-describe('CustomStoryPicker', () => {
-  it('should render', () => {
-    // Implement your tests here...
-  });
-});
