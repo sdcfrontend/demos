36f66590217e8d90b700b40e95a183f239a5b08a
Hawkin, Adam (Associate Software Developer) <adam.hawkin@sky.uk>
2018-01-09 10:50:13 +0000

    Added Custom Story Grid (WIP)

---
+++ custom-story-grid/src/test/components/custom-story-picker.test.js
@@ -0,0 +1,7 @@
+import CustomStoryPicker from '../../src/components/custom-story-picker';
+
+describe('CustomStoryPicker', () => {
+  it('should render', () => {
+    // Implement your tests here...
+  });
+});
