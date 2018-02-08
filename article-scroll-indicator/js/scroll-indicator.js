bff6652195ad96e24ecd6cf200b88d2a11dc32f4
Aidan Zealley <aidan.zealley@sky.uk>
2018-02-08 14:12:31 +0000

    change bar colours

--- article-scroll-indicator/js/scroll-indicator.js
+++ article-scroll-indicator/js/scroll-indicator.js
@@ -1,5 +1,3 @@
-NodeList.prototype.forEach = Array.prototype.forEach;
-
 function framePercentageValue(frameDimensions) {
 
   var value = '';
@@ -69,14 +67,14 @@ function addScrollIndicator() {
 
   var progressIndicator = document.createElement('div');
   progressIndicator.classList.add('progress-indicator');
-  progressIndicator.style.cssText = "position: fixed; top: 0; width: 100%; height: 5px; background-color: #eee; transform: none; transition: height 0.33s ease-in-out";
+  progressIndicator.style.cssText = "position: fixed; top: 0; width: 100%; height: 5px; background-color: #002672; transform: none; transition: height 0.33s ease-in-out";
 
   var progressBar = document.createElement('div');
   progressBar.classList.add('progress-indicator__bar');
   progressBar.setAttribute('data-parallax-role', 'subject');
   progressBar.setAttribute('data-actions', 'translateX');
   progressBar.setAttribute('data-start', 'top');
-  progressBar.style.cssText = "position: absolute; top: 0; left: -100%; width: 100%; height: 100%; background-color: #0072c9; will-change: transform;";
+  progressBar.style.cssText = "position: absolute; top: 0; left: -100%; width: 100%; height: 100%; background-color: #EA021A; will-change: transform;";
 
   progressIndicator.appendChild(progressBar);
   document.body.appendChild(progressIndicator);
