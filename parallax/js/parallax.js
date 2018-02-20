bcf9f88886b0fdd0cdf7395e1dfe0023b1e68cd8
Aidan Zealley <aidan.zealley@sky.uk>
2018-02-20 16:02:36 +0000

    add js fallback

--- parallax/js/parallax.js
+++ parallax/js/parallax.js
@@ -14,7 +14,7 @@ function framePercentageValue(frameDimensions, startFromBottom) {
 
 }
 
-function setProperties(parallaxFrames) {
+function setProperties() {
 
   var parallaxFrames = document.querySelectorAll('[data-parallax-role="frame"]');
 
@@ -22,14 +22,62 @@ function setProperties(parallaxFrames) {
 
     var parallaxSubjects = parallaxFrame.querySelectorAll('[data-parallax-role="subject"]');
 
-    parallaxSubjects.forEach(function(parallaxSubject) {
+    if (window.CSS.supports('--var', 0)) {
+      parallaxSubjects.forEach(function(parallaxSubject) {
 
-      parallaxSubject.classList.add('parallax-subject--' + parallaxSubject.getAttribute('data-parallax-property'));
-      parallaxSubject.style.setProperty('--parallaxMultiplier', parallaxSubject.getAttribute('data-parallax-multiplier'));
+        parallaxSubject.classList.add('parallax-subject--' + parallaxSubject.getAttribute('data-parallax-property'));
+        parallaxSubject.style.setProperty('--parallaxMultiplier', parallaxSubject.getAttribute('data-parallax-multiplier'));
 
-    });
+      });
+    }
 
   });
+
+}
+
+function setPropertyValue(parallaxSubject, value) {
+
+  var property = parallaxSubject.getAttribute('data-parallax-property');
+  var multiplier = 1
+
+  if (parallaxSubject.getAttribute('data-parallax-multiplier') !== null) {
+    multiplier = parallaxSubject.getAttribute('data-parallax-multiplier');
+  }
+
+  switch (property) {
+
+    case "translateY":
+      parallaxSubject.style.transform = 'translate3d(0,' + value * multiplier + 'px,0)';
+      break;
+
+    case "translateX":
+      parallaxSubject.style.transform = 'translate3d(' + value * multiplier + 'px,0,0)';
+      break;
+
+    case "rotate":
+      parallaxSubject.style.transform = 'rotate(' + value * multiplier + 'deg)';
+      break;
+
+    case "blur":
+      parallaxSubject.style.filter = 'blur(' + value * multiplier + 'px)';
+      break;
+
+    case "blur-reverse":
+      parallaxSubject.style.filter = 'blur(' + value * multiplier * -1 + 'px)';
+      break;
+
+    case "saturate":
+      if (value < 0) {
+        value = 0
+      }
+      parallaxSubject.style.filter = 'saturate(' + value * multiplier + '%)';
+      break;
+
+    case "fade":
+      parallaxSubject.style.opacity = (value * (multiplier/100));
+      break;
+
+  }
 
 }
 
@@ -48,15 +96,27 @@ function triggerParallax() {
 
         var parallaxSubjects = parallaxFrame.querySelectorAll('[data-parallax-role="subject"]');
 
-        parallaxSubjects.forEach(function(parallaxSubject) {
+        if (window.CSS.supports('--var', 0)) {
+          parallaxSubjects.forEach(function(parallaxSubject) {
 
-          if (parallaxSubject.dataset.parallaxStart == "bottom") {
-            parallaxSubject.style.setProperty('--frameScrollDepth', framePercentageValue(parallaxFrameDimensions, true));
-          } else {
-            parallaxSubject.style.setProperty('--frameScrollDepth', framePercentageValue(parallaxFrameDimensions, false));
-          }
+            if (parallaxSubject.dataset.parallaxStart == "bottom") {
+              parallaxSubject.style.setProperty('--frameScrollDepth', framePercentageValue(parallaxFrameDimensions, true));
+            } else {
+              parallaxSubject.style.setProperty('--frameScrollDepth', framePercentageValue(parallaxFrameDimensions, false));
+            }
 
-        });
+          });
+        } else {
+          parallaxSubjects.forEach(function(parallaxSubject) {
+
+            if (parallaxSubject.dataset.parallaxStart == "bottom") {
+              setPropertyValue(parallaxSubject, framePercentageValue(parallaxFrameDimensions, true));
+            } else {
+              setPropertyValue(parallaxSubject, framePercentageValue(parallaxFrameDimensions, false));
+            }
+
+          });
+        }
 
       }
 
