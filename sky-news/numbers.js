f31ad6b98790ef5aab2393d1b823e52d852e49fe
Groak, Mark <mark.groak@sky.uk>
2018-06-06 14:21:57 +0100

    add rewards animation

---
+++ sky-news/numbers.js
@@ -0,0 +1,92 @@
+NodeList.prototype.forEach = Array.prototype.forEach;
+
+function cleanNumber(number) {
+  var cleanDigits = [];
+  var dirtyDigits = number.split('');
+
+  dirtyDigits.forEach(function (digit, index) {
+
+    if (!isNaN(parseInt(digit, 10))) {
+      cleanDigits.push(digit);
+    }
+
+  });
+
+  return cleanDigits;
+}
+
+function createDigitsMarkup() {
+
+  var numberCounters = document.querySelectorAll('[data-role="number-counter"]');
+
+  numberCounters.forEach(function (numberCounter) {
+
+    var number = numberCounter.textContent;
+    var digits = number.split('');
+
+    numberCounter.setAttribute('data-number', number);
+    numberCounter.innerHTML = '';
+
+    digits.forEach(function (digit, index) {
+
+      if (isNaN(parseInt(digit, 10))) {
+        numberCounter.innerHTML += '<span class="number-counter__punctuation">' + digit + '</span>';
+      } else {
+        numberCounter.innerHTML += '<span class="number-counter__digit" data-role="digit"><span class="number-counter__digit-wheel" data-role="digit-wheel">098765432<span class="number-counter__digit--thin">1</span>0</span><span class="number-counter__digit--static">' + digit + '</span></span>';
+      }
+    });
+
+  });
+}
+
+function count(numberCounter) {
+
+  var digits = cleanNumber(numberCounter.dataset.number);
+  var digitElements = numberCounter.querySelectorAll('[data-role="digit"]');
+  var digitElementsArray = [];
+
+  digitElements.forEach(function (digitElement) {
+
+    digitElementsArray.push(digitElement);
+
+  });
+
+  digitElementsArray.forEach(function (digitElement, index) {
+    var digit = digits[index];
+    var digitWheel = digitElement.querySelector('[data-role="digit-wheel"]');
+
+    if (digit == 0) {
+      digit = 10;
+    }
+
+    digitWheel.style.cssText = 'transform: translateY(' + digit + 'em)';
+
+  });
+
+}
+
+function triggerCounters(numberCounters) {
+
+  function findScrollPosition() {
+
+    var height = window.innerHeight;
+
+    numberCounters.forEach(function (numberCounter) {
+
+      var dimensions = numberCounter.getBoundingClientRect();
+
+      if (dimensions.bottom < height) {
+        count(numberCounter);
+      }
+
+    });
+
+    requestAnimationFrame(findScrollPosition);
+
+  }
+
+  findScrollPosition();
+
+}
+
+createDigitsMarkup();
---
+++ sky-news/sf.js
@@ -0,0 +1,65 @@
+(function () {
+
+  window.slides = document.querySelectorAll('[data-role="slide"]');
+
+  function registerSlide(slide, style, func) {
+    slide[style] = func;
+  }
+
+  function triggerSlide(slide) {
+
+    var dims = slide.getBoundingClientRect();
+    if (dims.top < (window.innerHeight / 2)) {
+      return 'middle';
+    }
+
+    if (dims.top < (window.innerHeight)) {
+      return 'in';
+    }
+
+    return false;
+  }
+
+  function trackSlide(slide) {
+    var dims = slide.getBoundingClientRect();
+    var height = window.innerHeight;
+    if (dims.top < height && dims.top > (window.innerHeight * -1)) {
+      return Math.floor((100 / window.innerHeight) * dims.top);
+    }
+
+    return null;
+  }
+
+  function tick() {
+
+    for (var i = -1; ++i < slides.length;) {
+
+      var type = slides[i].getAttribute('data-type');
+      if (!type) {
+        return;
+      }
+
+      if (type === 'trigger' && slides[i].trigger) {
+        var tol = triggerSlide(slides[i]);
+        if (tol) {
+          slides[i].trigger(tol);
+        }
+      }
+
+      if (type === 'track' && slides[i].track) {
+        if (trackSlide(slides[i]) !== null) {
+          slides[i].track(trackSlide(slides[i]))
+
+        }
+      }
+    }
+
+    requestAnimationFrame(tick);
+  }
+
+  window.registerSlide = registerSlide;
+
+  tick();
+
+
+})()
