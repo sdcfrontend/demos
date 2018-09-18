665eb8dc8df7f9fb8c02f9bbda83a7c3083cd0ff
mark groak <markgroak@marks-MacBook.home>
2018-09-18 18:31:15 +0100

    remove scroll trackoing

--- us-politics/js/gerry.js
+++ us-politics/js/gerry.js
@@ -1,9 +1,45 @@
 (function () {
-  var con = document.querySelector('.sf-gerry__wrap');
+  var con = document.querySelector('.sf-gerry');
   var prec = con.querySelector('.sf-gerry__precints');
   var result = con.querySelector('[data-role="precints-won-result"]');
   var red = con.querySelector('[data-role="precints-won-red"]');
   var blue = con.querySelector('[data-role="precints-won-blue"]');
+  var links = con.querySelectorAll('[data-role="gerry-links"] a');
+
+  links.forEach(function (item) {
+    item.onclick = function (e) {
+
+      e.preventDefault();
+
+      links.forEach(function (link) {
+        link.removeAttribute('aria-current');
+      })
+
+      this.setAttribute('aria-current', true);
+      var state = item.getAttribute('data-step');
+      prec.setAttribute('data-state', state);
+
+      if (state === "0") {
+        result.textContent = "Red wins";
+        blue.textContent = "2";
+        red.textContent = "3";
+      }
+      if (state === "1") {
+        result.textContent = "Red wins";
+        blue.textContent = "0";
+        red.textContent = "5";
+
+
+      }
+
+      if (state === "2") {
+        result.textContent = 'Blue wins';
+        blue.textContent = "3";
+        red.textContent = "2";
+
+      }
+    }
+  });
 
   function tick() {
 
@@ -42,5 +78,5 @@
     requestAnimationFrame(tick);
   }
 
-  tick();
+  // tick();
 })()
