c1c4e60f14f9715a9b6795fdd7c481eca5f88140
Groak, Mark <mark.groak@sky.uk>
2018-09-21 15:28:59 +0100

    add midterms to presidents timeline

--- us-politics/js/graphs.js
+++ us-politics/js/graphs.js
@@ -13,6 +13,7 @@
 
   document.querySelectorAll('[data-role="gunsales-gfx"]').forEach(function (item) {
     item.start = function () {
+      item.init = true;
       this.removeAttribute('data-role');
       this.querySelectorAll('animate').forEach(function (animate) {
         animate.beginElement();
