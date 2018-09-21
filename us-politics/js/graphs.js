a61aa9eddb120783010d210682684c51d6bab3f9
Groak, Mark <mark.groak@sky.uk>
2018-09-21 09:29:53 +0100

    add gun sales gfx

---
+++ us-politics/js/graphs.js
@@ -0,0 +1,24 @@
+(function () {
+
+  document.querySelectorAll('[data-role="sf-line-graph"]').forEach(function (item) {
+
+    item.start = function () {
+      item.removeAttribute('data-role');
+      item.init = true;
+      item.querySelectorAll('animate').forEach(function (anim) {
+        anim.beginElement();
+      })
+    }
+  });
+
+  document.querySelectorAll('[data-role="gunsales-gfx"]').forEach(function (item) {
+    item.start = function () {
+      this.removeAttribute('data-role');
+      this.querySelectorAll('animate').forEach(function (animate) {
+        animate.beginElement();
+      })
+    }
+  })
+
+
+})()
