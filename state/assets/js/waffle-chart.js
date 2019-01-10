4eee9cca0fb9d92f167f94e43bb24c4e4226497a
groakm <mark.groak@sky.uk>
2019-01-10 15:36:03 +0000

    add accesibility attributes

--- state/assets/js/waffle-chart.js
+++ state/assets/js/waffle-chart.js
@@ -1,18 +1,24 @@
 function createWaffleChart(data){
 
-  var template = '<div class="waffle__chart-seg color-#{color}" data-bg data-trigger="100" style="transition-delay: #{delay}ms;" data-tooltip="#{value}%|#{label}" data-animate-in-view></div>';
+  var template = '<div class="waffle__chart-seg color-#{color}" data-bg data-trigger="100" style="transition-delay: #{delay}ms;" data-tooltip="#{value}%|#{label}" data-animate-in-view aria-hidden></div>';
   var html = '';
   var index = 0;
+  var summary = [];
 
   Object.keys(data).forEach(function(key){
     var item = data[key];
     for(var i = item.value;--i>-1;){
       index++;
       html += template.replace(/#{color}/, key).replace(/#{delay}/, (index*5)).replace(/#{value}/, item.value).replace(/#{label}/, item.label);
+      var text = item.label+ " " + item.value +"%";
+      if(summary.indexOf(text) === -1){
+        summary.push(text)
+      };
     }
+
   });
 
-  return html;
+  return html + '<p class="waffle__chart-summary">' + (summary.join(', ')) + '</p>';
 }
 
 function checkWaffleCharts(waffles){
