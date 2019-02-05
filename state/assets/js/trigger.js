0d101bc846e2da458793c0015542846916798330
mark groak <markgroak@marks-MacBook.home>
2019-02-05 21:12:29 +0000

    improve perforemance of segments

--- state/assets/js/trigger.js
+++ state/assets/js/trigger.js
@@ -32,27 +32,6 @@
       if (item.isIntersecting || item.intersectionRatio > 0) {
 
         if (item.target.hasAttribute('data-trigger') && item.intersectionRatio >= ratio) {
-
-          // if we want to trigger a function
-          if (item.target.start && !item.target.init) {
-            // observer.unobserve(item.target);
-            item.target.start();
-
-            // do the line below in the attached function (stops repeated invocations)
-            // item.target.init = true;
-          }
-
-          //if we want to play/pause a video
-          if (item.target.playpause) {
-            if (item.intersectionRatio > 0.25) {
-              item.target.playpause(item.target.querySelector('video'), true)
-            }
-
-            else {
-              item.target.playpause(item.target.querySelector('video'), false)
-            }
-          }
-
           if (item.target.hasAttribute('data-animate-in-view')) {
             observer.unobserve(item.target);
             item.target.removeAttribute('data-animate-in-view');
