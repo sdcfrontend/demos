aa3439929e935ca4b8d25cf475c861c188a9a068
mark groak <markgroak@marks-MacBook.home>
2019-02-05 22:00:53 +0000

    optimisation

--- state/assets/js/waffle-chart.js
+++ state/assets/js/waffle-chart.js
@@ -1,24 +1,21 @@
+var waffletemplate = '<div class="waffle__chart-seg color-#{color}" data-bg style="transition-delay: #{delay}ms;" data-tooltip="#{value}%|#{label}" aria-hidden data-waffle-seg></div>';
+
 function createWaffleChart(data) {
 
-  var template = '<div class="waffle__chart-seg color-#{color}" data-bg style="transition-delay: #{delay}ms;" data-tooltip="#{value}%|#{label}" aria-hidden data-waffle-seg></div>';
   var html = '';
   var index = 0;
   var summary = [];
-  var segment = 0;
 
   Object.keys(data).forEach(function (key) {
     var item = data[key];
     for (var i = item.value; --i > -1;) {
       index++;
-      html += template.replace(/#{color}/, key).replace(/#{delay}/, (index * 5)).replace(/#{value}/, item.value).replace(/#{label}/, item.label);
+      html += waffletemplate.replace(/#{color}/, key).replace(/#{delay}/, (index * 5)).replace(/#{value}/, item.value).replace(/#{label}/, item.label);
       var text = item.label + " " + item.value + "%";
       if (summary.indexOf(text) === -1) {
         summary.push(text)
       };
     }
-
-    segment++;
-
   });
 
   return html + '<p class="waffle__chart-summary">' + (summary.join(', ')) + '</p>';
