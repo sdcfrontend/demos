21a0babbd8396435d24a94fa5e7b6cd1fa4cf674
mark groak <markgroak@marks-MacBook.home>
2019-02-07 22:26:21 +0000

    optimisations

--- state/assets/js/trigger.js
+++ state/assets/js/trigger.js
@@ -1,8 +1,26 @@
 (function () {
+  if ('IntersectionObserver' in window) {
+    console.log('io');
 
-  var items = document.querySelectorAll('[data-trigger]');
+    var observer = new IntersectionObserver(scan, { rootMargin: '18px', threshold: [0, 0.25, 0.5, 0.75, 1] });
 
-  function tick() {
+    setTimeout(function () {
+      document.querySelectorAll('[data-trigger]').forEach(function (item) {
+        observer.observe(item);
+      });
+    }, 33);
+
+  }
+
+  else {
+    console.log('raf');
+    var items = document.querySelectorAll('[data-trigger]');
+    tick(items);
+
+  }
+
+
+  function tick(items) {
 
     for (var i = -1; ++i < items.length;) {
       var target = items[i];
@@ -19,41 +37,32 @@
     }
 
     setTimeout(function () {
-      requestAnimationFrame(tick);
+      requestAnimationFrame(function () {
+        tick(items)
+      });
     }, 50)
   }
 
-  tick();
+  function scan(entries, observer) {
 
-  //   var observer = new IntersectionObserver(scan, { rootMargin: '18px', threshold: [0, 0.25, 0.5, 0.75, 1] });
+    entries.forEach(function (item) {
+      var ratio = 0.25;
+      var trigger = item.target.getAttribute('data-trigger');
+      if (trigger && trigger.length) {
+        ratio = parseInt(trigger, 10) / 100;
+      }
 
-  //   setTimeout(function () {
-  //     document.querySelectorAll('[data-trigger]').forEach(function (item) {
-  //       observer.observe(item);
-  //     });
-  //   }, 100);
+      if (item.isIntersecting || item.intersectionRatio > 0) {
 
-
-  //   function scan(entries, observer) {
-
-  //     entries.forEach(function (item) {
-  //       var ratio = 0.25;
-  //       var trigger = item.target.getAttribute('data-trigger');
-  //       if (trigger && trigger.length) {
-  //         ratio = parseInt(trigger, 10) / 100;
-  //       }
-
-  //       if (item.isIntersecting || item.intersectionRatio > 0) {
-
-  //         if (item.target.hasAttribute('data-trigger') && item.intersectionRatio >= ratio) {
-  //           if (item.target.hasAttribute('data-animate-in-view')) {
-  //             observer.unobserve(item.target);
-  //             item.target.removeAttribute('data-animate-in-view');
-  //           }
-  //         }
-  //       }
-  //     })
-  //   }
+        if (item.target.hasAttribute('data-trigger') && item.intersectionRatio >= ratio) {
+          if (item.target.hasAttribute('data-animate-in-view')) {
+            observer.unobserve(item.target);
+            item.target.removeAttribute('data-animate-in-view');
+          }
+        }
+      }
+    })
+  }
 
 
 })();
