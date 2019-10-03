eb870718554f748b76d3929c6729e438ebf155b9
groakm <mark.groak@sky.uk>
2019-10-03 16:03:03 +0100

    add fetch to amp script example

--- amp-script/script.js
+++ amp-script/script.js
@@ -1,14 +1,23 @@
-const btn = document.querySelector('button');
-const input = document.querySelector('input');
-const div = document.querySelector('div');
+const btn = document.querySelector('.button');
+const input = document.querySelector('.input');
+const div = document.querySelector('.fancy-text');
 
 input.addEventListener('input', (event) => {});
 
 btn.addEventListener('click', () => {
   const input = document.querySelector('input');
-  div.textContent = input.value;
+
+  // div.textContent = input.value;
   div.classList.add('fancy-text--on');
+
   setTimeout(function(){
     div.classList.remove('fancy-text--on');
   }, 2000);
+
+  fetch('http://localhost:8080/data.json').then(function(res){
+    return res.json();
+  }).then(function(data){
+    div.textContent = JSON.stringify(data.mydata);
+  });
+
 });
