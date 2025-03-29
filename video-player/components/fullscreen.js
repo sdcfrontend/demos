845b0bf1d26000b597e6c0df3eb032c6b9993add
mark groak <markgroak@marks-MacBook-Air.local>
2025-03-29 18:37:54 +0000

    update player

--- video-player/components/fullscreen.js
+++
@@ -1,5 +0,0 @@
-export function fullscreen(config){
-    document.addEventListener("fullscreenchange", (event) => {
-        document.fullscreenElement === null ? config.controls.fullscreenButton.enterfs() : config.controls.fullscreenButton.exitfs()
-    });
-}
